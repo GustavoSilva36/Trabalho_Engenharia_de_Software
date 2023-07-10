81ffbf51f39f5fffea2d69fd8d4d78cf1884ec87
Rafael Brunini Pereira <bruninirafael@gmail.com>
2023-07-10 17:05:17 -0300

    Refactor: Arquivos de CORS para requisição

--- Source/BackEnd/app.js
+++ Source/BackEnd/app.js
@@ -21,6 +21,9 @@ app.all('', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost");
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  req.header("Access-Control-Allow-Origin", "http://localhost");
+  req.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  req.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   //Auth Each API Request created by user.
   next();
 });
