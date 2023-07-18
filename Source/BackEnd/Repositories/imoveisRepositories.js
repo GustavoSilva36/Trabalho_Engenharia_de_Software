ad924f877f0a3f77ba5026ea05208586db89b3f9
BryanNaneti <bryannanete951@gmail.com>
2023-07-18 00:07:13 -0300

    inclusao de Teste Unitário

--- Source/BackEnd/Repositories/imoveisRepositories.js
+++ Source/BackEnd/Repositories/imoveisRepositories.js
@@ -28,7 +28,7 @@ async function cadastrarImovel(imovel) {
       modelonegocio: imovel.modelonegocio,
       tipodeimovel: imovel.tipodeimovel,
     });
-    return novoImovel;
+    return "Imovel Cadastrado com sucesso";
   } catch (err) {
     console.log(err);
     throw err;
